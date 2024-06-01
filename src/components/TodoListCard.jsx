f451ac3e79df0885b059b5694c70c7a1b6daf90d
MarkMallari16 <mallariftw01@gmail.com>
2024-06-02 00:01:42 +0800

    add check icon

--- src/components/TodoListCard.jsx
+++ src/components/TodoListCard.jsx
@@ -5,6 +5,7 @@ function TodoListCard({ id, text, createdAt, onDelete, onEdit, onFinish, complet
 
     const formattedDate = moment(createdAt).format('ddd, MMM DD  YYYY');
     const currentTask = moment(createdAt).fromNow();
+
     const [editedText, setEditedText] = useState(text);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -14,6 +15,7 @@ function TodoListCard({ id, text, createdAt, onDelete, onEdit, onFinish, complet
             setIsEditing(false);
         }
     }
+
     return (
         <div className={`card w-96  bg-base-200 ring-1 ring-inset ${completed ? 'ring-green-500 ' : 'ring-gray-700'} mt-5`}>
             <div className="card-body text-start ">
@@ -28,7 +30,6 @@ function TodoListCard({ id, text, createdAt, onDelete, onEdit, onFinish, complet
                     <h1 className={`text-4xl font-bold text-wrap  ${completed && 'line-through w-full'} break-words`}>{text}</h1>
                 )}
                 <div className='flex flex-col  gap-2 text-gray-300'>
-
                     <div>{currentTask}</div>
 
                     <div className='flex items-center gap-1 mt-3'>
@@ -38,12 +39,14 @@ function TodoListCard({ id, text, createdAt, onDelete, onEdit, onFinish, complet
 
                         <div >{formattedDate}</div>
 
-
                     </div>
                     {completed &&
-                        <div className='flex gap-2 text-green-500 font-medium'>
-                            <span> Completed</span>
+                        <div className='flex gap-1 text-green-500 font-medium'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+                            </svg>
 
+                            <span>Completed</span>
                         </div>
                     }
                 </div>
