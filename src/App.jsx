9aafd80141b1fc5df1809f7b151f767c7fe20737
MarkMallari16 <mallariftw01@gmail.com>
2024-06-01 23:31:51 +0800

    add second

--- src/App.jsx
+++ src/App.jsx
@@ -173,8 +173,6 @@ function App() {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
                   </svg>
-
-
                   Sort by</div>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-30">
                   <li><a onClick={() => setSortOrder('ascending')} className={sortOrder === 'ascending' ? 'bg-base-300' : ''}>Ascending</a></li>
@@ -210,7 +208,7 @@ function App() {
           </div>
         ) : (
           <div className='flex justify-center w-full'>
-            <div className='bg-base-300 rounded-lg flex flex-col  justify-center items-center py-5 w-full  mt-2 text-gray-300 gap-1'>
+            <div className='bg-base-300 rounded-lg flex flex-col justify-center items-center py-10 w-full  mt-2 text-gray-300 gap-1'>
               <span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
--- src/components/TodoListCard.jsx
+++ src/components/TodoListCard.jsx
@@ -4,6 +4,7 @@ import moment from 'moment';
 function TodoListCard({ id, text, createdAt, onDelete, onEdit, onFinish, completed }) {
 
     const formattedDate = moment(createdAt).format('ddd, MMM DD  YYYY');
+    const currentTask = moment(createdAt).fromNow();
     const [editedText, setEditedText] = useState(text);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -13,7 +14,6 @@ function TodoListCard({ id, text, createdAt, onDelete, onEdit, onFinish, complet
             setIsEditing(false);
         }
     }
-
     return (
         <div className={`card w-96  bg-base-200 ring-1 ring-inset ${completed ? 'ring-green-500 ' : 'ring-gray-700'} mt-5`}>
             <div className="card-body text-start ">
@@ -27,12 +27,17 @@ function TodoListCard({ id, text, createdAt, onDelete, onEdit, onFinish, complet
                 ) : (
                     <h1 className={`text-4xl font-bold text-wrap  ${completed && 'line-through w-full'} break-words`}>{text}</h1>
                 )}
-                <div className='flex items-center gap-2 text-gray-300'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-                    </svg>
+                <div className='flex flex-col  gap-2 text-gray-300'>
 
-                    <span>{formattedDate}</span>
+                    <div>{currentTask}</div>
+
+                    <div className='flex items-center gap-1 mt-3'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                        </svg>
+
+                        <div >{formattedDate}</div>
+                    </div>
                     {completed &&
                         <div className='flex gap-2 text-green-500 font-medium'>
                             <span> Completed</span>
