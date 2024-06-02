429d123c8f67d5f83faa54a0a2a88d98143c9667
MarkMallari16 <mallariftw01@gmail.com>
2024-06-02 10:07:26 +0800

    add tab index in dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -176,30 +176,30 @@ function App() {
           </div>
           <div className='flex '>
             <div>
-              <div className="dropdown dropdown-hover">
-                <div role='button' className="m-1 btn">
+              <div className="dropdown">
+                <div tabIndex={0} role='button' className="m-1 btn">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
                   </svg>
                   <span className='hidden lg:block'>Sort by</span>
                 </div>
-                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-30">
-                  <li><a onClick={() => setSortOrder('ascending')} className={sortOrder === 'ascending' ? 'bg-base-300' : ''}>Ascending</a></li>
-                  <li><a onClick={() => setSortOrder('descending')} className={sortOrder === 'descending' ? 'bg-base-300' : ''}>Descending</a></li>
+                <ul tabIndex={0} className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-30">
+                  <li><span onClick={() => setSortOrder('ascending')} className={sortOrder === 'ascending' ? 'bg-base-300' : ''}>Ascending</span></li>
+                  <li><span onClick={() => setSortOrder('descending')} className={sortOrder === 'descending' ? 'bg-base-300' : ''}>Descending</span></li>
                 </ul>
               </div>
             </div>
             <div>
-              <div className="dropdown dropdown-end lg:dropdown-end dropdown-hover">
-                <div role='button' className="m-1 btn">
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role='button' className="m-1 btn">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
                   </svg>
 
                   <span className='hidden lg:block'>Filter by</span></div>
                 <ul tabIndex={0} className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-30">
-                  <li><a onClick={() => setFilter('all')} className={filter === 'all' ? 'bg-base-300' : ''}>All</a></li>
-                  <li><a onClick={() => setFilter('active')} className={filter === 'active' ? 'bg-base-300' : ''}>Active</a></li>
+                  <li><span onClick={() => setFilter('all')} className={filter === 'all' ? 'bg-base-300' : ''}>All</span></li>
+                  <li><span onClick={() => setFilter('active')} className={filter === 'active' ? 'bg-base-300' : ''}>Active</span></li>
                   <li><a onClick={() => setFilter('completed')} className={filter === 'completed' ? 'bg-base-300' : ''}>Completed</a></li>
                 </ul>
               </div>
